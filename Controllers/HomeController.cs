001ec420732a1b4368eebd3360f5f2a479e96925
daniel karll <danieljkarlL@gmail.com>
2020-01-27 19:22:21 -0600

    Added pagination

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Modas.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace Modas.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly int PageSize = 20;
+
         private IEventRepository repository;
         public HomeController(IEventRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult Index() => View(repository.Events);
+        public ViewResult Index(int page = 1) => View(
+            repository.Events.Include(e => e.Location)
+            .OrderBy(e => e.TimeStamp)
+            .Skip((page - 1) * PageSize)
+            .Take(PageSize));
     }
 }
